0026932888ace8f9071fcd45bf698a0626bf9a71
emotzart <johnshuabratsk@gmail.com>
2022-11-06 02:30:34 +0700

    added lesson data importer and created command

--- ads/management/commands/import_data.py
+++ ads/management/commands/import_data.py
@@ -1,19 +1,16 @@
+import json
 from pathlib import Path
 from django.core.management.base import BaseCommand
-from ads.models import Category, Advertisement, Location
-from users.models import User
-from ads.management.data import load_csv_as_json
+from ads.models import Category, Advertisement
+from ads.management.data import compile_jsons
 
 JSONS_PATH = Path(__file__).parent.parent.absolute().joinpath('data', 'datasets')
-CATEGORIES_FILE_CSV = JSONS_PATH.joinpath('categories.csv')
-ADVERTISEMENTS_FILE_CSV = JSONS_PATH.joinpath('ads.csv')
-LOCATIONS_FILE_CSV = JSONS_PATH.joinpath('location.csv')
-USERS_FILE_CSV = JSONS_PATH.joinpath('user.csv')
-
-
+CATEGORIES_FILE = JSONS_PATH.joinpath('categories.json')
+ADVERTISEMENTS_FILE = JSONS_PATH.joinpath('ads.json')
 class Command(BaseCommand):
     def import_categories(self):
-        data = load_csv_as_json(CATEGORIES_FILE_CSV)
+        with open(CATEGORIES_FILE) as file:
+            data = json.load(file)
 
         for item in data:
             item.pop('id')
@@ -24,7 +21,8 @@ class Command(BaseCommand):
         print('Categories was imported')
 
     def import_advertisements(self):
-        data = load_csv_as_json(ADVERTISEMENTS_FILE_CSV)
+        with open(ADVERTISEMENTS_FILE) as file:
+            data = json.load(file)
 
         for item in data:
             item.pop('id')
@@ -34,30 +32,8 @@ class Command(BaseCommand):
             new_ad.save()
         print('Advertisements was imported')
 
-    def import_locations(self):
-        data = load_csv_as_json(LOCATIONS_FILE_CSV)
-
-        for item in data:
-            item.pop('id')
-
-            new_ad = Location()
-            [setattr(new_ad, key, value) for key, value in item.items()]
-            new_ad.save()
-        print('Locations was imported')
-
-    def import_users(self):
-        data = load_csv_as_json(USERS_FILE_CSV)
-        for item in data:
-            item.pop('id')
-
-            new_ad = User()
-            [setattr(new_ad, key, value) for key, value in item.items()]
-            new_ad.save()
-        print('Users was imported')
-
 
     def handle(self, *args, **options):
-        self.import_locations()
+        compile_jsons()
         self.import_categories()
-        self.import_users()
         self.import_advertisements()
