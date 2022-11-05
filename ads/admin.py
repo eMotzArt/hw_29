f3389d30a16a2d669fd225dc92ac9a669fa001dc
emotzart <johnshuabratsk@gmail.com>
2022-11-06 02:27:33 +0700

    added app: ads

--- ads/admin.py
+++ ads/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Advertisement, Category, Location
-# Register your models here.
 
-admin.site.register(Advertisement)
-admin.site.register(Category)
-admin.site.register(Location)
+# Register your models here.
