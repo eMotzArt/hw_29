e6c55ef8fac4c672e324799067c2ef9a4a1db413
emotzart <johnshuabratsk@gmail.com>
2022-11-14 19:29:23 +0700

    Reworked import_data command

--- ads/management/commands/import_data.py
+++ ads/management/commands/import_data.py
@@ -1,12 +1,14 @@
 import json
 from pathlib import Path
 from django.core.management.base import BaseCommand
-from ads.models import Category, Advertisement
+from ads.models import Category, Advertisement, User, Location
 from ads.management.data import compile_jsons
 
 JSONS_PATH = Path(__file__).parent.parent.absolute().joinpath('data', 'datasets')
 CATEGORIES_FILE = JSONS_PATH.joinpath('categories.json')
 ADVERTISEMENTS_FILE = JSONS_PATH.joinpath('ads.json')
+LOCATIONS_FILE = JSONS_PATH.joinpath('location.json')
+USERS_FILE = JSONS_PATH.joinpath('user.json')
 class Command(BaseCommand):
     def import_categories(self):
         with open(CATEGORIES_FILE) as file:
@@ -32,8 +34,32 @@ class Command(BaseCommand):
             new_ad.save()
         print('Advertisements was imported')
 
+    def import_locations(self):
+        with open(LOCATIONS_FILE, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        for item in data:
+            item.pop('id')
+
+            new_ad = Location()
+            [setattr(new_ad, key, value) for key, value in item.items()]
+            new_ad.save()
+        print('Locations was imported')
+
+    def import_users(self):
+        with open(USERS_FILE) as file:
+            data = json.load(file)
+        for item in data:
+            item.pop('id')
+
+            new_ad = User()
+            [setattr(new_ad, key, value) for key, value in item.items()]
+            new_ad.save()
+        print('Users was imported')
+
 
     def handle(self, *args, **options):
         compile_jsons()
         self.import_categories()
+        self.import_locations()
+        self.import_users()
         self.import_advertisements()
--- ads/models.py
+++ ads/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    lat = models.FloatField()
+    lng = models.FloatField()
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    role = models.CharField(max_length=20)
+    age = models.PositiveSmallIntegerField()
+    location = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
+
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
@@ -12,10 +26,11 @@ class Category(models.Model):
 
 class Advertisement(models.Model):
     name = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.PositiveIntegerField()
     description = models.CharField(max_length=2000)
-    address = models.CharField(max_length=500)
+    image = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     is_published = models.BooleanField(default=False)
 
     def get_dict(self):
@@ -25,9 +40,5 @@ class Advertisement(models.Model):
             'author': self.author,
             'price': self.price,
             'description': self.description,
-            'address': self.address,
             'is_published': self.is_published,
         }
-
-
-
