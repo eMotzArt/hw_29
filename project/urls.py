0e197be917a305d20482f550b5d310f7ed15c574
emotzart <johnshuabratsk@gmail.com>
2022-11-21 00:51:19 +0700

    added framework to project

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,7 @@ import ads.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', ads.views.index),
     path('', include('ads.urls')),
     path('', include('users.urls')),
