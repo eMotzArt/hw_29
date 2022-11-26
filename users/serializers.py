edf8037c56efaac6be7fc57124c3efd718f7b594
emotzart <johnshuabratsk@gmail.com>
2022-11-26 19:18:40 +0700

    Added serializers for user

--- users/serializers.py
+++ users/serializers.py
@@ -6,3 +6,22 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'slug', 'first_name', 'last_name', 'username', 'password', 'role', 'age', 'location']
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    location = serializers.CharField(read_only=True)
+    class Meta:
+        model = User
+        fields = ['id', 'slug', 'first_name', 'last_name', 'username', 'password', 'role', 'age', 'location']
+
+class UserDestroySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id']
