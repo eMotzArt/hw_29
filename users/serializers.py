b89616c9a77564512fb91e953a7be74e3eccf6c8
emotzart <johnshuabratsk@gmail.com>
2022-11-28 15:46:48 +0700

    Added total_published_ads for users

--- users/serializers.py
+++ users/serializers.py
@@ -3,9 +3,14 @@ from users.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     location = serializers.SlugRelatedField(read_only=True, slug_field='name')
+    total_published_ads = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ['id', 'slug', 'first_name', 'last_name', 'username', 'password', 'role', 'age', 'location']
+        fields = ['id', 'slug', 'first_name', 'last_name', 'username', 'password', 'role', 'age', 'location', 'total_published_ads']
+
+    def get_total_published_ads(self, user):
+        return user.advertisement_set.filter(is_published=True).count()
+
 
 class UserCreateSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(required=False)
